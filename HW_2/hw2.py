fdf3aa1a749024032b80bf071bc729aa0f70f3bb
Marvin-Deng <52214624+Marvin-Deng@users.noreply.github.com>
2024-04-09 19:10:21 -0700

    feat: Add DFS function, comments, and tests for 2

--- HW_2/hw2.py
+++ HW_2/hw2.py
@@ -1,6 +1,15 @@
 """
 HW 2, Marvin Deng
 
+1. BFS traversal of the tuple tree can be done using a list representing a queue. 
+   The queue contains the next subtree in the level to process. We process the queue one level at a time.
+   When we reach a leaf node (not a tuple), we can add it to our result.
+   When we reach another tuple, we add its children into the queue.
+
+2. DFS traversal of the tuple tree can be done using a list representing a stack. 
+   The stack contains the next subtree in the level to process. We pop from the end of the list.
+   When we reach a leaf node (not a tuple), we can add it to our result.
+   When we reach another tuple, we append its children to the back of the list.
 
 """
 
@@ -12,7 +21,7 @@ def BFS(TREE):
     - TREE (tuple): A tuple representing a tree
 
     Returns:
-    - tuple: The tuple representing the BFS traversal of TREE
+    - tuple: The tuple representing the left to right BFS traversal of TREE
     """
     if not isinstance(TREE, tuple):
         return (TREE,)
@@ -30,3 +39,29 @@ def BFS(TREE):
                 queue.extend(curr)
 
     return tuple(bfs_order)
+
+
+def DFS(TREE):
+    """
+    2.
+    
+    Args:
+        - TREE (tuple): A tuple representing a tree
+
+    Returns:
+        - tuple: The tuple representing a left to right DFS traversal of TREE
+    """
+    if not isinstance(TREE, tuple):
+        return (TREE,)
+
+    dfs_order = []
+    stack = [TREE]
+
+    while stack:
+        curr = stack.pop()
+        if isinstance(curr, tuple):
+            stack.extend(curr[::-1])
+        else:
+            dfs_order.append(curr)
+
+    return tuple(dfs_order)
--- HW_2/test_hw2.py
+++ HW_2/test_hw2.py
@@ -1,6 +1,7 @@
 import unittest
 from hw2 import (
     BFS,
+    DFS,
 )
 
 class TestBFS(unittest.TestCase):
@@ -21,6 +22,30 @@ class TestBFS(unittest.TestCase):
 
     def test_BFS_example6(self):
         self.assertEqual(BFS(("A", (("C", (("E",), "D")), "B"))), ('A', 'B', 'C', 'D', 'E'))
+
+class TestDFS(unittest.TestCase):
+
+    def test_example1(self):
+        self.assertEqual(DFS("ROOT"), ('ROOT',))
+
+    def test_example2(self):
+        self.assertEqual(DFS(((("L", "E"), "F"), "T")), ('L', 'E', 'F', 'T'))
+
+    def test_example3(self):
+        self.assertEqual(DFS(("R", ("I", ("G", ("H", "T"))))), ('R', 'I', 'G', 'H', 'T'))
+
+    def test_example4(self):
+        self.assertEqual(DFS((("A", ("B",)), ("C",), "D")), ('A', 'B', 'C', 'D'))
+
+    def test_example5(self):
+        self.assertEqual(DFS(("T", ("H", "R", "E"), "E")), ('T', 'H', 'R', 'E', 'E'))
+
+    def test_example6(self):
+        self.assertEqual(DFS(("A", (("C", (("E",), "D")), "B"))), ('A', 'C', 'E', 'D', 'B'))
+
+    def test_example4(self):
+        self.assertEqual(DFS(("A", "B", "C", "D")), ('A', 'B', 'C', 'D'))
+
 
 if __name__ == '__main__':
     unittest.main()
