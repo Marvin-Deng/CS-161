65aad2e1751504be635eed258165f6d99f8b2235
Marvin-Deng <52214624+Marvin-Deng@users.noreply.github.com>
2024-04-10 19:32:34 -0700

    hw2: Reformat comment headers for FINAL_STATE and NEXT_STATE

--- HW_2/hw2.py
+++ HW_2/hw2.py
@@ -101,46 +101,66 @@ def DFID(TREE, D):
 
     return tuple(result)
 
+"""
+4.
 
-# These functions implement a depth-first solver for the homer-baby-dog-poison
-# problem. In this implementation, a state is represented by a single tuple
-# (homer, baby, dog, poison), where each variable is True if the respective entity is
-# on the west side of the river, and False if it is on the east side.
-# Thus, the initial state for this problem is (False False False False) (everybody
-# is on the east side) and the goal state is (True True True True).
+These functions implement a depth-first solver for the homer-baby-dog-poison
+problem. In this implementation, a state is represented by a single tuple
+(homer, baby, dog, poison), where each variable is True if the respective entity is
+on the west side of the river, and False if it is on the east side.
+Thus, the initial state for this problem is (False False False False) (everybody
+is on the east side) and the goal state is (True True True True).
 
-# The main entry point for this solver is the function DFS_SOL, which is called
-# with (a) the state to search from and (b) the path to this state. It returns
-# the complete path from the initial state to the goal state: this path is a
-# list of intermediate problem states. The first element of the path is the
-# initial state and the last element is the goal state. Each intermediate state
-# is the state that results from applying the appropriate operator to the
-# preceding state. If there is no solution, DFS_SOL returns [].
-# To call DFS_SOL to solve the original problem, one would call
-# DFS_SOL((False, False, False, False), [])
-# However, it should be possible to call DFS_SOL with any intermediate state (S)
-# and the path from the initial state to S (PATH).
+The main entry point for this solver is the function DFS_SOL, which is called
+with (a) the state to search from and (b) the path to this state. It returns
+the complete path from the initial state to the goal state: this path is a
+list of intermediate problem states. The first element of the path is the
+initial state and the last element is the goal state. Each intermediate state
+is the state that results from applying the appropriate operator to the
+preceding state. If there is no solution, DFS_SOL returns [].
+To call DFS_SOL to solve the original problem, one would call
+DFS_SOL((False, False, False, False), [])
+However, it should be possible to call DFS_SOL with any intermediate state (S)
+and the path from the initial state to S (PATH).
+"""
 
-# First, we define the helper functions of DFS_SOL.
+"""
+FINAL_STATE
 
-# FINAL_STATE takes a single argument S, the current state, and returns True if it
-# is the goal state (True, True, True, True) and False otherwise.
+Args:
+- S, the current state, and returns True if it
+
+Returns:
+- If the goal state (True, True, True, True) and False otherwise.
+"""
+
 def FINAL_STATE(S):
-    raise NotImplementedError
+    return S == (True, True, True, True)
 
+"""
+NEXT_STATE returns the state that results from applying an operator to the
+current state. 
 
-# NEXT_STATE returns the state that results from applying an operator to the
-# current state. It takes two arguments: the current state (S), and which entity
-# to move (A, equal to "h" for homer only, "b" for homer with baby, "d" for homer
-# with dog, and "p" for homer with poison).
-# It returns a list containing the state that results from that move.
-# If applying this operator results in an invalid state (because the dog and baby,
-# or poisoin and baby are left unsupervised on one side of the river), or when the
-# action is impossible (homer is not on the same side as the entity) it returns [].
-# NOTE that NEXT_STATE returns a list containing the successor state (which is
-# itself a tuple)# the return should look something like [(False, False, True, True)].
+Args:
+- the current state (S)
+- which entity to move (A, equal to "h" for homer only, "b" for homer with baby, "d" for homer
+with dog, and "p" for homer with poison).
+
+Returns:
+- A list containing the state that results from that move.
+- If applying this operator results in an invalid state (because the dog and baby,
+or poisoin and baby are left unsupervised on one side of the river), or when the
+action is impossible (homer is not on the same side as the entity) it returns [].
+
+NOTE that NEXT_STATE returns a list containing the successor state (which is
+itself a tuple)# the return should look something like [(False, False, True, True)].
+"""
+
 def NEXT_STATE(S, A):
-    raise NotImplementedError
+    if A == "h":
+        new_tuple = (False,) + tuple(S[1:])
+
+
 
 
 # SUCC_FN returns all of the possible legal successor states to the current
